1b069a4a2b63b6c5cd4d1bcbcce8ee3af204f93f
Yazmina Yolanda Yepes <110237967+yyyepes@users.noreply.github.com>
2024-05-11 18:08:07 -0500

    Update db.sql
    ;)

--- database/db.sql
+++ database/db.sql
@@ -1,17 +1,16 @@
-Create database bakery;
+CREATE DATABASE bakery;
+USE bakery;
 
-use bakery;
-
-create table users(
-    id INT(11) NOT NULL,
-    username varchar(16) NOT NULL,
-    password varchar(60) NOT NULL,
-    fullname varchar(100) NOT NULL,
+CREATE TABLE users(
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    username VARCHAR(16) NOT NULL,
+    password VARCHAR(60) NOT NULL,
+    fullname VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
 );
-alter table users
-    modify column id INT(11) NOT NULL AUTO_INCREMENT =1;
 
-
+ALTER TABLE users
+MODIFY COLUMN id INT(11) NOT NULL AUTO_INCREMENT=1;
 
 CREATE TABLE Producto (
     Código INT PRIMARY KEY,
@@ -24,7 +23,11 @@ CREATE TABLE Producto (
 CREATE TABLE Cliente (
     Código INT PRIMARY KEY,
     Dirección VARCHAR(255),
-    Teléfono VARCHAR(20)
+    Teléfono VARCHAR(20),
+    Usuario VARCHAR(50),
+    Nombre VARCHAR(100),
+    Apellido VARCHAR(100),
+    FOREIGN KEY (Código) REFERENCES users(id)
 );
 
 CREATE TABLE Domiciliario (
@@ -34,5 +37,37 @@ CREATE TABLE Domiciliario (
     Horario_disponibilidad VARCHAR(100),
     Medio_transporte VARCHAR(50),
     Licencia_conducción VARCHAR(50),
-    Fecha_vencimiento_licencia DATE
+    Fecha_vencimiento_licencia DATE,
+    FOREIGN KEY (Código) REFERENCES users(id)
+);
+
+CREATE TABLE Tienda (
+    Código INT PRIMARY KEY,
+    Dirección VARCHAR(255)
+);
+
+CREATE TABLE Pedido (
+    Código INT PRIMARY KEY,
+    Fecha_hora_envío DATETIME,
+    Fecha_hora_fin_servicio DATETIME,
+    Dirección_entrega VARCHAR(255),
+    Tipo_recogida ENUM('En tienda', 'A domicilio'),
+    Código_cliente INT,
+    Código_domiciliario INT,
+    Código_tienda INT,
+    FOREIGN KEY (Código_cliente) REFERENCES Cliente(Código),
+    FOREIGN KEY (Código_domiciliario) REFERENCES Domiciliario(Código),
+    FOREIGN KEY (Código_tienda) REFERENCES Tienda(Código)
+);
+
+CREATE TABLE Compra (
+    Código INT PRIMARY KEY,
+    Cantidad INT,
+    Fecha_compra DATE,
+    Código_cliente INT,
+    Código_producto INT,
+    Código_pedido INT,
+    FOREIGN KEY (Código_cliente) REFERENCES Cliente(Código),
+    FOREIGN KEY (Código_producto) REFERENCES Producto(Código),
+    FOREIGN KEY (Código_pedido) REFERENCES Pedido(Código)
 );
