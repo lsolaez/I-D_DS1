23c47f871c559b1cb4ee1ab492d91c99e0d89be2
Leonardo Solaez <lsolaez@uninorte.edu.co>
2024-05-29 13:07:18 -0500

    validando funciones que requieren estar logeados

--- src/routes/links.js
+++ src/routes/links.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../database');
+const { isLoggedIn } = require('../lib/auth');
 
-router.get('/add', (req, res) => {
+router.get('/add',isLoggedIn, (req, res) => {
     res.render('links/add', { script: '' });
 });
 
-router.post('/add', async (req, res) => {
+router.post('/add',isLoggedIn, async (req, res) => {
     const { id, imagen, nombre, descripcion, precio, categoria, disponible } = req.body;
 
     // Validación de código único
@@ -34,31 +35,31 @@ router.post('/add', async (req, res) => {
     res.render('links/add', { script });
 });
 
-router.get('/', async (req, res) => {
+router.get('/', isLoggedIn, async (req, res) => {
     const productos = await pool.query('SELECT * FROM Producto');
     res.render('links/list', { productos });
 });
 
 //new para interfaz de usuario
-router.get('/listUsers', async (req, res) => {
+router.get('/listUsers',isLoggedIn, async (req, res) => {
     const productos = await pool.query('SELECT * FROM Producto');
     res.render('links/listUsers', { productos });
 });
 
-router.get('/delete/:id', async (req, res) => {
+router.get('/delete/:id',isLoggedIn, async (req, res) => {
     const { id } = req.params;
     await pool.query('DELETE FROM producto WHERE id = ?', [id]);
     req.flash('success', 'Product Removed Successfully');
     res.redirect('/links');
 });
 
-router.get('/edit/:id', async (req, res) => {
+router.get('/edit/:id',isLoggedIn, async (req, res) => {
     const { id } = req.params;
     const productos = await pool.query('SELECT * FROM producto WHERE id = ?', [id]);
     res.render('links/edit', { productos: productos[0] });
 });
 
-router.post('/edit/:id', async (req, res) => {
+router.post('/edit/:id',isLoggedIn, async (req, res) => {
     const { id } = req.params;
     const { imagen, nombre, descripcion, precio, categoria, disponible } = req.body;
     const updatedProduct = { imagen, nombre, descripcion, precio, categoria, disponible };
@@ -68,7 +69,7 @@ router.post('/edit/:id', async (req, res) => {
 });
 
 // Agregar producto al carrito
-router.post('/cart/add/:id', async (req, res) => {
+router.post('/cart/add/:id',isLoggedIn, async (req, res) => {
     const { id } = req.params;
     const producto = await pool.query('SELECT * FROM producto WHERE id = ?', [id]);
 
@@ -93,11 +94,11 @@ router.post('/cart/add/:id', async (req, res) => {
 });
 
 // Mostrar carrito
-router.get('/cart', (req, res) => {
+router.get('/cart',isLoggedIn, (req, res) => {
     res.render('links/cart', { cart: req.session.cart });
 });
 
-router.post('/cart/delete/:cartId', (req, res) => {
+router.post('/cart/delete/:cartId',isLoggedIn, (req, res) => {
     const { cartId } = req.params;
     const cartIndex = req.session.cart.findIndex(item => item.cartId == cartId);
     if (cartIndex > -1) {
@@ -109,7 +110,7 @@ router.post('/cart/delete/:cartId', (req, res) => {
 });
 
 // Finalizar compra
-router.post('/cart/checkout', async (req, res) => {
+router.post('/cart/checkout',isLoggedIn, async (req, res) => {
     const user = req.user;
     if (!user) {
         return res.json({ success: false, message: 'Usuario no autenticado.' });
